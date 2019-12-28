ff9c926b4e8ca0919342bcfe80af5d7f6697b2e3
Juan <juanignacio.fherran@gmail.com>
2019-12-28 19:02:23 +0100

    added training labels to dataset

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -29,8 +29,10 @@ def identify_log_events(df: pandas.DataFrame) -> pandas.DataFrame:
     # identify consecutive fog events and generates a unique label
     df.loc[:, 'groups'] = df.loc[df['fog'] == 1, 'time'].diff().ge(1.5).cumsum()
     # TODO think about return just a series rather than a whole dataframe or part of it
+
     # return everything but time vars
     return df.loc[:, [column for column in df.columns if column not in [0, 1, 'fog', 'time']]]
+
 
 def extract_sequences(df: pandas.DataFrame, window_lenght: int, fog_event_lenght:int,
                       id_generator: Generator, pca_model) -> pandas.DataFrame:
@@ -41,6 +43,7 @@ def extract_sequences(df: pandas.DataFrame, window_lenght: int, fog_event_lenght
     unique_fog_events_labels = df['groups'].unique()
     fog_events = unique_fog_events_labels[~numpy.isnan(unique_fog_events_labels)]
     acumulated_sequences = []
+    # TODO reformat this function
     valid_sequences = 0
     for fog_event in fog_events:
         if len(df.loc[df['groups'] == fog_event]) >= fog_event_lenght:
@@ -64,17 +67,33 @@ def extract_sequences(df: pandas.DataFrame, window_lenght: int, fog_event_lenght
     else:
         return pandas.concat(acumulated_sequences, ignore_index=True)
 
-def extract_labels():
-    pass
+
+def extract_labels(df: pandas.DataFrame, fog_event_lenght) -> pandas.DataFrame:
+    # get number of events
+    unique_fog_events_labels = df['groups'].unique()
+    fog_events = unique_fog_events_labels[~numpy.isnan(unique_fog_events_labels)]
+    acumulated_labels = list()
+    # TODO reformat this function b
+    for fog_event in fog_events:
+        group_df = df.loc[df['groups'] == fog_event]
+        if len(group_df) >= fog_event_lenght:
+
+            # y is the minimum RVR in the fog event
+            y = group_df[23].min()
+            acumulated_labels.append(y)
+    return acumulated_labels
+
+
 
 def id_generator() -> Iterator[int]:
     """
-    Dummy generator
+    Dummy generator to keep track of the sequences
     """
     id = 0
     while True:
         yield id
         id += 1
+
 
 def extract_characteristics_from_sequences(df: pandas.DataFrame, settings) -> pandas.DataFrame:
     """
@@ -106,6 +125,7 @@ def main(input_path: str, output_path: str):
     generator = id_generator()
 
     all_sequences = list()
+    labels = list()
     settings = tsfresh.feature_extraction.settings.MinimalFCParameters()
     mat = src.utils.commons.read_mat(input_path)
     datasets = src.utils.commons.concat_dataset(mat)
@@ -115,17 +135,24 @@ def main(input_path: str, output_path: str):
                              if column not in [0, 1, 23]]])
     for dataset in src.utils.commons.dataset_generator(mat):
         dataset_with_fog_events = identify_log_events(dataset)
+        labels.extend(extract_labels(dataset_with_fog_events, fog_event_lenght=2))
         dataset_sequences = extract_sequences(dataset_with_fog_events, window_lenght=10, fog_event_lenght=2,
                                               id_generator=generator, pca_model=pca)
         all_sequences.append(dataset_sequences)
+
 
     # generate a single DataFrame with all the sequences
     datasets_sequences = pandas.concat(all_sequences, ignore_index=False)
 
     datasets_features = extract_characteristics_from_sequences(datasets_sequences.dropna(), settings=settings)
+
+    # add labels to dataset
+    datasets_features.loc[:, 'y'] = labels
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     datasets_features.to_csv(output_path + 'features.csv', sep=';', index=True)
+
+    # TODO generate labels
 
 
 if __name__ == '__main__':
