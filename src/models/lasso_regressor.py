34c88de62db7fe0ea724ab6afbf5ebe1e8a284ce
Juan <juanignacio.fherran@gmail.com>
2020-05-27 21:12:44 +0200

    added lasso model

---
+++ src/models/lasso_regressor.py
@@ -0,0 +1,14 @@
+import sklearn.linear_model
+import src.models.base_model
+
+
+class RidgeRegressor(src.models.base_model.BaseModel):
+
+    @property
+    def estimator(self):
+        # TODO add alpha from config file
+        return sklearn.linear_model.Lasso()
+
+    @property
+    def estimator_name(self):
+        return 'Lasso Regression'
