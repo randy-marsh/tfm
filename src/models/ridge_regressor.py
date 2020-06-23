7d7b362b6290627c642a07176853dce2e6f9804b
Juan <juanignacio.fherran@gmail.com>
2020-06-24 00:11:47 +0200

    solved bug with rdge regressor

--- src/models/ridge_regressor.py
+++ src/models/ridge_regressor.py
@@ -7,8 +7,13 @@ class RidgeRegressor(src.models.base_model.BaseModel):
 
     @property
     def estimator(self):
-        config = configparser.ConfigParser().read('ridge.cfg')
-        return sklearn.linear_model.Ridge(alpha=config.getfloat('DEFAULT', 'alpha'))
+        try:
+            config = configparser.RawConfigParser()
+            config.read('ridge.cfg')
+            return sklearn.linear_model.Ridge(alpha=config.getfloat('DEFAULT', 'alpha'))
+        except configparser.NoOptionError or FileNotFoundError:
+            # TODO warning or logging
+            return sklearn.linear_model.Ridge()
 
     @property
     def estimator_name(self):
