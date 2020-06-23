face71bf2c24581d0e698963217c016ad7c07ad8
Juan <juanignacio.fherran@gmail.com>
2020-06-23 23:58:01 +0200

    added elastic net configuration file

--- src/models/elastic_net_regressor.py
+++ src/models/elastic_net_regressor.py
@@ -1,13 +1,17 @@
 import sklearn.linear_model
 import src.models.base_model
+import configparser
 
 
 class ElasticNetRegressor(src.models.base_model.BaseModel):
 
     @property
     def estimator(self):
-        # TODO add alpha from config file
-        return sklearn.linear_model.ElasticNet()
+        config = configparser.ConfigParser().read('elastic_net.cfg')
+        alpha = config.getfloat('DEFAULT', 'alpha')
+        l1_ratio = config.getfloat('DEFAULT', 'l1_ratio')
+        return sklearn.linear_model.ElasticNet(alpha=alpha, l1_ratio=l1_ratio)
+
     @property
     def estimator_name(self):
         return 'Elastic Net Regression'
