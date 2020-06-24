fdc4173debf2f7fbf59ea3b092af5475c2d044d1
Juan <juanignacio.fherran@gmail.com>
2020-06-24 19:46:05 +0200

    added congif file for neural network

--- src/models/nn_regressor.py
+++ src/models/nn_regressor.py
@@ -1,13 +1,24 @@
 import sklearn.neural_network
 import src.models.base_model
-
+import configparser
 
 class NNRegressor(src.models.base_model.BaseModel):
 
     @property
     def estimator(self):
-        # TODO add config
-        return sklearn.neural_network.MLPRegressor()
+        try:
+            #TODO there is a bug
+            config = configparser.RawConfigParser()
+            config.read('nn.cfg')
+            alpha = config.getfloat('DEFAULT', 'alpha')
+            hidden_layer_sizes = config.getint('DEFAULT', 'hidden_layer_sizes')
+            solver = config.get('DEFAULT', 'solver')
+            early_stopping = config.getboolean('DEFAULT', 'early_stopping')
+            return sklearn.neural_network.MLPRegressor(alpha=alpha, hidden_layer_sizes=hidden_layer_sizes, max_iter=500000,
+                                                       solver=solver, early_stopping=early_stopping)
+        except configparser.NoOptionError or FileNotFoundError:
+            # TODO warning or logging
+            return sklearn.neural_network.MLPRegressor()
 
     @property
     def estimator_name(self):
