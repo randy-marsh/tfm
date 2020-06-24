6fbd36cb19b774883350f372e1095ab001f43dd7
Juan <juanignacio.fherran@gmail.com>
2020-06-24 17:57:52 +0200

    added default config for svr regressor

--- src/models/svr_regressor.py
+++ src/models/svr_regressor.py
@@ -1,14 +1,22 @@
 import sklearn.svm
 import src.models.base_model
+import configparser
 
 
 class SVRRegressor(src.models.base_model.BaseModel):
 
     @property
     def estimator(self):
-        # TODO add config
-        return sklearn.svm.SVR()
-
+        try:
+            config = configparser.RawConfigParser()
+            config.read('knn.cfg')
+            kernel = config.get('DEFAULT', 'kernel')
+            C = config.getfloat('DEFAULT', 'C')
+            epsilon = config.getfloat('DEFAULT', 'epsilon')
+            return sklearn.svm.SVR(kernel=kernel, C=C, epsilon=epsilon)
+        except configparser.NoOptionError or FileNotFoundError:
+            # TODO warning or logging
+            return sklearn.svm.SVR()
     @property
     def estimator_name(self):
         return 'SVR Regression'
