e91b05f0780513d15b703640edbf87cc3a5ce9be
Juan <juanignacio.fherran@gmail.com>
2020-06-24 20:10:04 +0200

    added random forest configuration

--- src/models/random_forest_regressor.py
+++ src/models/random_forest_regressor.py
@@ -1,13 +1,22 @@
 import sklearn.ensemble
 import src.models.base_model
+import configparser
 
 
 class RandomForestRegressor(src.models.base_model.BaseModel):
 
     @property
     def estimator(self):
-        # TODO add hyperparameters from config file
-        return sklearn.ensemble.RandomForestRegressor()
+        try:
+            config = configparser.RawConfigParser()
+            config.read('random_forest.cfg')
+            n_estimators = config.getint('DEFAULT', 'n_estimators')
+            max_depth = config.getint('DEFAULT', 'max_depth')
+            return sklearn.ensemble.RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth)
+        except configparser.NoOptionError or FileNotFoundError:
+            # TODO warning or logging
+            return sklearn.ensemble.RandomForestRegressor()
+
 
     @property
     def estimator_name(self):
