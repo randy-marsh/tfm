a070d3d7fe722bd67db36b9083baef00ff1577b9
Juan <juanignacio.fherran@gmail.com>
2020-06-26 14:07:08 +0200

    chanded to avoid code duplication

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -19,18 +19,17 @@ def rmse(y_true: numpy.ndarray, y_pred: numpy.ndarray) -> float:
     return numpy.sqrt(sklearn.metrics.mean_squared_error(y_true=y_true, y_pred=y_pred))
 
 
-def threshold_accuracy_1000(y_true: numpy.ndarray, y_pred: numpy.ndarray) -> float:
+def threshold_accuracy(y_true: numpy.ndarray, y_pred: numpy.ndarray, thr: float or int) -> float:
     """
     Umbralizes the input given a threshold and compute the accuracy
     :param numpy.ndarray y_true: Ground truth (correct) target values.
     :param numpy.ndarray y_pred: Estimated target values.
+    :param thr: threshold
     :return float: accuracy estimation
     """
-    threshold = 1000
-    y_true_thr = numpy.where(y_true <= threshold, 1, 0)
-    y_pred_thr = numpy.where(y_pred <= threshold, 1, 0)
+    y_true_thr = numpy.where(y_true <= thr, 1, 0)
+    y_pred_thr = numpy.where(y_pred <= thr, 1, 0)
     return sklearn.metrics.accuracy_score(y_true=y_true_thr, y_pred=y_pred_thr)
-
 
 class BaseModel(abc.ABC):
 
